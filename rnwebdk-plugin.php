327c350d5241ff07e6d62b0355d26bf436353eec
Rnwebdk <Cashamir@gmail.com>
2017-09-24 13:43:47 +0200

    Create a WordPress Plugin from Scratch - Part 1

---
+++ rnwebdk-plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ *@package  rnwebdkPlugin
+ * 
+*/
+/*
+Plugin Name: rnwebdk Plugin
+Plugin URI: http://rnweb.dk
+Description: Learning plugin development with alessendaro from youtube
+Version 1.0.0
+Author: Rami "rnwebdk" Noureddine
+Author URI: http://rnweb.dk
+License: GPLv2 or later
+Text Domain: rnwebdk-plugin
+*/
+
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. if not, write to the free software
+Foundation, Inc., S1 Franklin street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+ */
